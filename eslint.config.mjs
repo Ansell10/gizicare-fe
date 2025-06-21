a258610dca45c3260dacfeab686ab57129ddcfb9
Ansell10 <syomeronansell@gmail.com>
2025-06-21 21:31:45 +0700

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,21 @@
-import { defineConfig } from "eslint-define-config";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default defineConfig({
-  plugins: [
-    "next", // Menambahkan plugin Next.js
-    "@typescript-eslint", // Menambahkan plugin TypeScript
-    "react" // Menambahkan plugin React
-  ],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off", // Mematikan aturan no-explicit-any
-    "react/no-unescaped-entities": "off", // Mematikan aturan react/no-unescaped-entities
-    "@typescript-eslint/no-unused-vars": "warn", // Menambahkan aturan no-unused-vars
-    "react-hooks/exhaustive-deps": "warn", // Menambahkan aturan exhaustive-deps
-    "next/next/no-img-element": "warn" // Menambahkan aturan no-img-element
-  }
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
 });
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  { rules: { "@typescript-eslint/no-explicit-any": "off",
+    "react/no-unescaped-entities": "off"
+} },
+  
+];
+
+export default eslintConfig;
