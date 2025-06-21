a748bebcd7740c1e0bdfa54b1bf9355e6efc2128
Ansell10 <syomeronansell@gmail.com>
2025-06-21 22:17:08 +0700

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,6 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-// Mengimpor aturan ESLint menggunakan import
-import eslintRecommended from 'eslint/conf/eslint-recommended.js'; 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -12,16 +9,27 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: Object.fromEntries(
-      Object.keys(eslintRecommended.rules).map(rule => [
-        rule,
-        "warn",
-      ])
-    ),
-  },
-];
+const eslintConfig = {
+  overrides: [
+    {
+      files: ['*.js', '*.ts', '*.tsx'], // Menetapkan aturan untuk file JS, TS, dan TSX
+      extends: [
+        "next/core-web-vitals",    // Menambahkan aturan Next.js untuk web vitals
+        "next/typescript"          // Menambahkan aturan untuk TypeScript di Next.js
+      ],
+      plugins: [
+        "next",                    // Plugin untuk Next.js
+        "@typescript-eslint"       // Plugin untuk TypeScript
+      ],
+      rules: {
+        "@typescript-eslint/no-explicit-any": "off", // Nonaktifkan aturan no-explicit-any
+        "react/no-unescaped-entities": "off",        // Nonaktifkan aturan no-unescaped-entities
+        "@typescript-eslint/no-unused-vars": "warn", // Menambahkan aturan no-unused-vars
+        "react-hooks/exhaustive-deps": "warn",      // Menambahkan aturan exhaustive-deps untuk React hooks
+        "next/next/no-img-element": "warn"          // Menambahkan aturan no-img-element untuk penggunaan gambar di Next.js
+      }
+    }
+  ]
+};
 
 export default eslintConfig;
