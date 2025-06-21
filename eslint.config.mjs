ef7e27b96a0833ca599bba0e332698acddd5c266
Ansell10 <syomeronansell@gmail.com>
2025-06-21 22:50:50 +0700

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,31 +5,21 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = {
-  overrides: [
-    {
-      files: ['*.js', '*.ts', '*.tsx'], // Menetapkan aturan untuk file JS, TS, dan TSX
-      extends: [
-        "next/core-web-vitals",    // Menambahkan aturan Next.js untuk web vitals
-        "next/typescript"          // Menambahkan aturan untuk TypeScript di Next.js
-      ],
-      plugins: [
-        "next",                    // Plugin untuk Next.js
-        "@typescript-eslint"       // Plugin untuk TypeScript
-      ],
-      rules: {
-        "@typescript-eslint/no-explicit-any": "off", // Nonaktifkan aturan no-explicit-any
-        "react/no-unescaped-entities": "off",        // Nonaktifkan aturan no-unescaped-entities
-        "@typescript-eslint/no-unused-vars": "warn", // Menambahkan aturan no-unused-vars
-        "react-hooks/exhaustive-deps": "warn",      // Menambahkan aturan exhaustive-deps untuk React hooks
-        "next/next/no-img-element": "warn"          // Menambahkan aturan no-img-element untuk penggunaan gambar di Next.js
-      }
-    }
-  ]
-};
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  { rules: { "@typescript-eslint/no-explicit-any": "off",
+    "react/no-unescaped-entities": "off",
+    "@typescript-eslint/no-unused-vars":"off",
+    "@typescript-eslint/no-unused-expressions":"off",
+    "@typescript-eslint/no-require-imports":"off",
+
+} },
+  
+];
 
 export default eslintConfig;
