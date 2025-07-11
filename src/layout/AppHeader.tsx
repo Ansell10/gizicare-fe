14284ee3a4056cf1f437cb1955be2420ce0a2180
Ansell10 <syomeronansell@gmail.com>
2025-07-11 20:00:17 +0700

    update notif

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -196,7 +196,7 @@ const AppHeader: React.FC = () => {
             isApplicationMenuOpen ? "flex" : "hidden"
           } items-center justify-between w-full gap-4 px-5 py-4 lg:flex shadow-theme-md lg:justify-end lg:px-0 lg:shadow-none`}
         >
-          <div className="flex items-center gap-2 2xsm:gap-2">
+          <div className="flex items-right gap-0.5 2xsm:gap-0.5">
             <NotificationDropdown />
           </div>
           <UserDropdown />
